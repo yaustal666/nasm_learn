bf4781f5b7d612d166cfa6dccc68d95cd0ee3fdf
yaustal666 <max.poz@mail.ru>
2023-08-11 07:28:21 +1100

    first func

---
+++ first_func/print_but_func.asm
@@ -0,0 +1,41 @@
+; THe same printing program but using function
+;
+;---------------------------------------------
+
+section	.data
+
+msg:	db	"Hello, world! It's me!",	10
+
+section	.text
+global	_start
+
+strlen:
+	push		rcx				; func strlen use rcx as counter
+	mov		rcx,		rsi		; answer will be left in rbx
+							; cause length of string must be in rbx to call write
+nextchar:						; I could use rdx as counter and leave answer in it
+							; but for lessons i wanted to use push and pop
+	cmp	byte	[rcx],		0		; which would not be needed if i used rdx
+	jz		finish				; and it would be kinda unsafe i guess
+	inc		rcx				; I could leave a comment though, something like
+	jmp		nextchar			; 'Attention you will louse your data in rbx if you use this func'
+
+finish:
+
+	sub		rcx,		rsi
+	mov		rdx,		rcx
+	pop		rcx
+	ret
+
+_start:
+
+	mov		rsi,		msg
+	call		strlen
+
+	mov		rdi,		1
+	mov		rax,		1
+	syscall
+
+	mov		rax,		60
+	xor		rdi,		rdi
+	syscall
--- hello_asm/hello.asm
+++ hello_asm/hello.asm
@@ -4,18 +4,20 @@
 
 section .data
 
-msg:	db	"Hello, World", 10
+msg:
+	db	"Hello, World", 10
 
 section	.text
 
 	global	_start
 
-_start:	mov	rax,	1		; system call for write
-	mov	rdi,	1		; file descriptor 1 - standart output
-	mov	rsi,	msg
-	mov	rdx,	13		; size of string in bytes
+_start:
+	mov		rax,		1		; system call for write
+	mov		rdi,		1		; file descriptor 1 - standart output
+	mov		rsi,		msg
+	mov		rdx,		13		; size of string in bytes
 	syscall
 
-	mov	rax,	60		; system call for exit
-	xor	rdi,	rdi		; rdi = 0, exit code
+	mov		rax,		60		; system call for exit
+	xor		rdi,		rdi		; rdi = 0, exit code
 	syscall
